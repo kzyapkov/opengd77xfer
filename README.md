df0aee0aa2ee11efd2679f809129151e53e7ee63
Kiril Zyapkov <kiril.zyapkov@gmail.com>
2020-07-16 09:59:00 +0300

    Add full GPLv2 license; update copyright notices

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
 # GD77transfer.py
 
+## Overview
+
+I use Linux and prefer open-source. OpenGD77 is great, but there is no
+proper codeplug-editing solution for Linux. This here tries to do that.
+
+## Copyright
+
 This code was originally published on
 
 http://www.opengd77.com/viewtopic.php?f=12&t=959&p=8847&hilit=python+linux&sid=6edaf5a7e67d939886e47aaeaa26dadf#p8829
 
-by user oh1fss
+by user oh1fss, licensed under GPLv2.
 
-### Notes to Self
-
-Serial command handling in firmware:
-
-    firmware/source/usb/usb_com.c; static void cpsHandleCommand(void)
+    Copyright (C) 2020  OH1FSS, juhani.jaakola@iki.fi
+                F1RMB, Daniel Caujolle-Bert <f1rmb.daniel@gmail.com>
+                VK3KYY / G4KYF, Roger Clark
+                LZ1CK, Kiril Zyapkov
--- gd77xfer.py
+++ gd77xfer.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Copyright (C) 2020  OH1FSS juhani.jaakola@iki.fi
+Copyright (C) 2020  OH1FSS, juhani.jaakola@iki.fi
                     F1RMB, Daniel Caujolle-Bert <f1rmb.daniel@gmail.com>
-                    VK3KYY / G4KYF, Roger Clark.
+                    VK3KYY / G4KYF, Roger Clark
+                    LZ1CK, Kiril Zyapkov
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
 as published by the Free Software Foundation; either version 2
-of the License.
+of the License, or (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
